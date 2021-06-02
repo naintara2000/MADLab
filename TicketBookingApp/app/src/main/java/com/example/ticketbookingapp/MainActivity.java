330dcceee309cc1fa70a84caad0c32b3499c63a1
naintara2000 <45703106+naintara2000@users.noreply.github.com>
2021-06-02 12:30:20 +0530

    Source and Destination Spinner

--- TicketBookingApp/app/src/main/java/com/example/ticketbookingapp/MainActivity.java
+++ TicketBookingApp/app/src/main/java/com/example/ticketbookingapp/MainActivity.java
@@ -5,14 +5,27 @@ package com.example.ticketbookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker picker;
+    Button displayDate;
+    TextView textview1;
+    TimePicker timePicker;
+    Button buttonDate;
+    Button buttonTime;
+    TextView textDate;
+    TextView textTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +39,37 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapterSS = ArrayAdapter.createFromResource(this, R.array.places_array, android.R.layout.simple_spinner_item);
         adapterSS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerS.setAdapter(adapterSS);
+        buttonDate = findViewById(R.id.buttonDate);
+        buttonTime = findViewById(R.id.buttonTime);
+        textDate = findViewById(R.id.textDate);
+        textTime = findViewById(R.id.textTime);
+        //timePicker=(TimePicker)findViewById(R.id.timePicker);
+        //Uncomment the below line of code for 24 hour view
+        timePicker.setIs24HourView(true);
+        textview1 = (TextView) findViewById(R.id.textView1);
+        //picker = (DatePicker) findViewById(R.id.datePicker);
+
+        displayDate=(Button)findViewById(R.id.buttonSelect);
+
+
+
+        buttonDate.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                final DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int time) {
+
+                    }
+                }, 0, 0,0 );
+                datePickerDialog.show();
+            }
+        });
+
     }
 
     public void SelectButton(android.view.View view) {
+        //textview1.setText("Current Date: "+getCurrentDate() + "Time: " + getCurrentTime());
         String destText = ((Spinner)findViewById(R.id.spinnerDestinations)).getSelectedItem().toString();
         String sourceText = ((Spinner)findViewById(R.id.spinnerSource)).getSelectedItem().toString();
         Toast.makeText(
@@ -46,7 +87,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-}
+    /*public String getCurrentDate(){
+        StringBuilder builder=new StringBuilder();;
+        builder.append((picker.getMonth() + 1)+"/");//month is 0 based
+        builder.append(picker.getDayOfMonth()+"/");
+        builder.append(picker.getYear());
+        return builder.toString();
+    }
 
-//public class MyOnItemSelectedListener implements AdapterView.OnItemClickListener {
-//}
+    public String getCurrentTime(){
+        String currentTime=timePicker.getCurrentHour()+":"+timePicker.getCurrentMinute();
+        return currentTime;
+    }*/
+
+}
